05d03623aaaccbb0c8a977eca7981c4898df3aa5
CoderJS007 <ajabezsamuel01@gmail.com>
2025-05-21 22:40:07 +0530

    update HelloController to accept requestParam

--- src/main/java/com/jabez/hello/HelloController.java
+++ src/main/java/com/jabez/hello/HelloController.java
@@ -1,13 +1,14 @@
 package com.jabez.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
-    public String sayHello() {
-        return "Hello from Spring Boot on Render!";
+    @GetMapping("/greet")
+    public String greetUser(@RequestParam(defaultValue = "Guest") String name) {
+        return "Hello + " + name;
 
     }
     
